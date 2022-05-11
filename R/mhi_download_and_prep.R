c6e2f4db0f11ccfe821c9264c40ec1e9f44f0805
Clay Ford <joe.clayton.ford@gmail.com>
2022-05-11 09:40:27 -0400

    add mhi script and data

---
+++ R/mhi_download_and_prep.R
@@ -0,0 +1,13 @@
+# Download median household income by census tract in Albemarle County
+
+library(tidycensus)
+library(dplyr)
+library(stringr)
+
+# vars <- load_variables(2020, "acs5", cache = TRUE)
+# median household income
+mhi <- get_acs(geography = "tract", variables = "B19013_001",
+               state = "VA", county = "Albemarle", year = 2020)
+mhi <- mhi %>% 
+  mutate(CensusTract = str_extract(NAME, pattern = "[0-9]{3}\\.?[0-9]{0,2}"))
+write.csv(mhi, file = "data/median_household_income.csv", row.names = FALSE)
